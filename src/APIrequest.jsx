78f6da09f6af593fd766c49ea83055f1fe90074e
iamolegk1 <oleg.kropivka5@gmail.com>
2022-02-16 19:07:45 +0200

    Added skeleton

--- src/APIrequest.jsx
+++ src/APIrequest.jsx
@@ -6,15 +6,33 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import StorageIcon from "@mui/icons-material/Storage";
+import ImageListItem from "@mui/material/ImageListItem";
+import Stack from "@mui/material/Stack";
+import Skeleton from "@mui/material/Skeleton";
+import { height } from "@mui/system";
 
 function APIrequest() {
-  const [value, setValue] = React.useState(() => []);
+  const [posts, setPosts] = React.useState([]);
+  const [loading, setLoading] = React.useState(false);
 
-  React.useEffect(() => {
-    axios.get("https://jsonplaceholder.typicode.com/posts").then(({ data }) => {
-      setValue(data);
-    });
-  }, []);
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.typicode.com/photos"
+      );
+      if (!data) return;
+      setPosts(data.slice(0, 2000));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const Item = styled(Paper)(({ theme }) => ({
     ...theme.typography.body2,
@@ -23,22 +41,55 @@ function APIrequest() {
     color: theme.palette.text.secondary,
   }));
 
+  let arrSkeleton = new Array(10).fill(
+    <Stack>
+      <Skeleton variant="text" sx={{ width: "300px" }} />
+      <Skeleton variant="text" sx={{ width: "300px" }} />
+      <Skeleton variant="rectangular" width={300} height={350} />
+    </Stack>
+  );
+
   return (
     <>
       <CssBaseline />
       <Box sx={{ p: 2 }}>
+        <Stack direction="row" justifyContent="center" alignItems="center">
+          <Button
+            disabled={loading}
+            onClick={fetchData}
+            color="info"
+            variant="contained"
+            size="medium"
+            endIcon={<StorageIcon />}
+          >
+            Data
+          </Button>
+        </Stack>
         <Grid
           container
           spacing={{ xs: 2, md: 2 }}
           columns={{ xs: 4, sm: 8, md: 10 }}
         >
-          {value.map((item) => (
-            <Grid item xs={8} sm={4} md={2} key={item.id}>
-              <Item>
-                {item.title} {item.body}
-              </Item>
-            </Grid>
-          ))}
+          {posts &&
+            posts.map((item) => (
+              <Grid item xs={8} sm={4} md={2} key={item.id}>
+                <Item>
+                  <Typography variant="h6" gutterBottom>
+                    {item.title}
+                  </Typography>
+                  <Divider />
+                  <ImageListItem key={item.id}>
+                    <img src={item.url} alt={item.title} loading="lazy" />
+                  </ImageListItem>
+                </Item>
+              </Grid>
+            ))}
+          {loading &&
+            arrSkeleton.map((item, index) => (
+              <Grid item xs={8} sm={4} md={2} key={index}>
+                {item}
+              </Grid>
+            ))}
         </Grid>
       </Box>
     </>
